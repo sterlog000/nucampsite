a670460d497246cec7c119852cefefb9ab0c73de
Logan Sterling <sterlog000@gmail.com>
2021-04-16 23:28:41 -0700

    React Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments({comments}) {
         if (comments) {
@@ -21,7 +22,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -36,8 +36,18 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             return (
                 <div className="container">
                     <div className="row">
-                        <RenderCampsite campsite = {props.campsite} />
-                        <RenderComments comments = {props.campsite.comments}/>
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
