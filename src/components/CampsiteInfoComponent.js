b5c968366661b0ad0edbbe72c38f8c274e63972e
Logan Sterling <sterlog000@gmail.com>
2021-04-17 12:34:27 -0700

    Workshop 3 fixed date

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -9,7 +9,7 @@ function RenderComments({ comments }) {
                 <h4>
                     Comments
                   </h4>
-                {comments.map(comment => <div key={comment.id}> <p> {comment.text} <br /> {comment.author} - {comment.date} </p> </div>)}
+                {comments.map(comment => <div key={comment.id} > <p> {comment.text} <br /> {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p> </div>)}
             </div>
         )
     }
