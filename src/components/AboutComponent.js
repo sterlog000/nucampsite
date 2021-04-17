98d30ff29b8e5aafee19597b09fb87bf1cdf1143
Logan Sterling <sterlog000@gmail.com>
2021-04-17 12:25:10 -0700

    Workshop 3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
+function RenderPartner({ partner }) {
+    if (!!partner) {
+        return (
+            <React.Fragment>
+                <Media object src={partner.image} alt={partner.name} width="150" />
+                <Media body className={"ml-5 mb-4"}>
+                    <Media heading> {partner.name} </Media>
+                    <Media heading> {partner.description} </Media>
+                </Media>
+            </React.Fragment>
+        );
+    } return(<div />);
+}
 
 function About(props) {
-
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner = {partner} />
+            </Media>
         );
     });
 
@@ -65,7 +78,7 @@ function About(props) {
                     <h3>Community Partners</h3>
                 </div>
                 <div className="col mt-4">
-                    <Media list>
+                    <Media list={true}>
                         {partners}
                     </Media>
                 </div>
@@ -74,4 +87,4 @@ function About(props) {
     );
 }
 
-export default About; 
+export default About;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import About from './AboutComponent'
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
